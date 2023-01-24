9bea9d2688fa5eb27bb4c468e93974fce54d644d
Kqyang29 <yangkq0417@163.com>
2023-01-23 23:02:48 -0800

    only search left

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -25,7 +25,9 @@ function Profile() {
 
       return unsub;
     }
-  }, [db, query]);
+
+
+  }, [db, query, session]);
 
 
 
@@ -70,111 +72,107 @@ function Profile() {
   // console.log(likevideo);
   return (
     <div>
-      {session ? (
-        <>
-          <Navbar />
-          <div className="flex max-w-7xl mx-auto">
-            <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-              {/* Sidebar */}
-              <Sidebar />
-            </div>
 
-            {/* main */}
-            <div className="mt-4 flex flex-col  overflow-auto h-[88vh] videos flex-1">
-              <div className="flex flex-col h-full w-full">
+      <Navbar />
+      <div className="flex max-w-7xl mx-auto">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          {/* Sidebar */}
+          <Sidebar />
+        </div>
 
-                {/* top */}
-                <div className='space-y-10'>
-                  <div className='flex space-x-5 items-center ml-10'>
-                    <img
-                      src={user?.userImg} alt=""
-                      className='rounded-full w-14 h-14 lg:w-20 lg:h-20'
-                    />
+        {/* main */}
+        <div className="mt-4 flex flex-col  overflow-auto h-[88vh] videos flex-1">
+          <div className="flex flex-col h-full w-full">
 
-                    <div>
-                      <p className='flex items-center font-semibold text-xl gap-2'>
-                        {user?.name}
-                        <GoVerified className='text-blue-500' />
-                      </p>
-                      <p className='text-gray-500'>
-                        {user?.username}
-                        {user?.id?.slice(0, 4)}
-                      </p>
-                    </div>
-                  </div>
+            {/* top */}
+            <div className='space-y-10'>
+              <div className='flex space-x-5 items-center ml-10'>
+                <img
+                  src={user?.userImg} alt=""
+                  className='rounded-full w-14 h-14 lg:w-20 lg:h-20'
+                />
 
-
-                  <div className='flex items-center space-x-10 text-gray-500 text-xl border-b'>
-                    <p
-                      onClick={() => setshowVideo(true)}
-                      className={`ml-14 cursor-pointer ${showVideo && `border-b-2 border-black font-bold text-black`}`}
-                    >
-                      Videos
-                    </p>
-
-                    <p
-                      onClick={() => setshowVideo(false)}
-                      className={`ml-14 cursor-pointer ${!showVideo && `border-b-2 border-black font-bold text-black`}`}
-                    >
-                      Liked
-                    </p>
-                  </div>
+                <div>
+                  <p className='flex items-center font-semibold text-xl gap-2'>
+                    {user?.name}
+                    <GoVerified className='text-blue-500' />
+                  </p>
+                  <p className='text-gray-500'>
+                    {user?.username}
+                    {user?.id?.slice(0, 4)}
+                  </p>
                 </div>
+              </div>
 
 
-                {/* bottom */}
-                {showVideo ? (
-                  <>
-                    {videos?.length > 0 ? (
-                      <div className="flex  flex-wrap justify-start gap-5 w-full h-full overflow-y-scroll px-12">
+              <div className='flex items-center space-x-10 text-gray-500 text-xl border-b'>
+                <p
+                  onClick={() => setshowVideo(true)}
+                  className={`ml-14 cursor-pointer ${showVideo && `border-b-2 border-black font-bold text-black`}`}
+                >
+                  Videos
+                </p>
 
-                        {videos.map(video => (
-
-                          <VideoList
-                            video={video}
-                            key={video.videoId}
-                            videoId={video.videoId}
-                          />
-
-                        ))}
-
-
-                      </div>
-                    ) : (
-                      <NoResult text="No results found!" />
-                    )}
-                  </>
-                ) : (
-
-
-                  <>
-                    {likevideo?.length > 0 ? (
-                      <div className="flex  flex-wrap justify-start gap-5 w-full h-full overflow-y-scroll px-12">
-                        {likevideo.map(video => (
-                          <VideoList
-                            key={video.videoId}
-                            video={video}
-
-                          />
-                        ))}
-
-                      </div>
-                    ) : (
-                      <NoResult text="No results found!" />
-                    )}
-                  </>
-                )}
-
-
-
-
+                <p
+                  onClick={() => setshowVideo(false)}
+                  className={`ml-14 cursor-pointer ${!showVideo && `border-b-2 border-black font-bold text-black`}`}
+                >
+                  Liked
+                </p>
               </div>
             </div>
+
+
+            {/* bottom */}
+            {showVideo ? (
+              <>
+                {videos?.length > 0 ? (
+                  <div className="flex  flex-wrap justify-start gap-5 w-full h-full overflow-y-scroll px-12">
+
+                    {videos.map(video => (
+
+                      <VideoList
+                        video={video}
+                        key={video.videoId}
+                        videoId={video.videoId}
+                      />
+
+                    ))}
+
+
+                  </div>
+                ) : (
+                  <NoResult text="No results found!" />
+                )}
+              </>
+            ) : (
+
+
+              <>
+                {likevideo?.length > 0 ? (
+                  <div className="flex  flex-wrap justify-start gap-5 w-full h-full overflow-y-scroll px-12">
+                    {likevideo.map(video => (
+                      <VideoList
+                        key={video.videoId}
+                        video={video}
+
+                      />
+                    ))}
+
+                  </div>
+                ) : (
+                  <NoResult text="No results found!" />
+                )}
+              </>
+            )}
+
+
+
+
           </div>
-        </>
-      ) : (
-        router.push("/")
-      )}
+        </div>
+      </div>
+
     </div>
   )
 }
